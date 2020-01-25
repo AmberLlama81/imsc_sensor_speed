c5d4d64644a729757cbe09e03792ac69e77e3baf
Nathan Huh <nhuh@usc.edu>
2020-01-25 11:41:00 -0800

    Re-programming SQL part with Raw SQL

--- update.py
+++ update.py
@@ -1,6 +1,5 @@
 import json
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 from sklearn.metrics import mean_squared_error
 from pandas import read_csv
 from pandas import datetime
@@ -8,56 +7,20 @@ from pandas import DataFrame
 from statsmodels.tsa.arima_model import ARIMA
 from matplotlib import pyplot
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+db_string = "postgresql://adms-api:V^YDiwdR&VvM@localhost/PostgreSQL"
 
-def parser(x):
-	return datetime.strptime('120'+x, '%Y-%m')
-
-class Sensor(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    logitude = db.Column(db.Float, unique=False, nullable = False)
-    latitude = db.Column(db.Float, unique=False, nullable = False)
-
-    def __repr__(self):
-        return super().__repr__()   
+db = create_engine(db_string)
 
 # Change the querying statements
-Sensor.query.all()
- 
- # Need temp file to store the past history of 2 minutes
-series = read_csv('sensor_speed.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-X = series.values
-size = int(len(X) * 0.66)
-train, test = X[0:size], X[size:len(X)]
-history = [x for x in train]
-predictions = list()
+query_result = db.execute("SELECT link_id, start_location.lat, start_location.lng FROM congestion_inventory")
 
-for t in range(len(test)):
-	model = ARIMA(history, order=(5,1,0))
-	model_fit = model.fit(disp=0)
-	output = model_fit.forecast()
-	yhat = output[0]
-	predictions.append(yhat)
-	obs = test[t]
-	history.append(obs)
-	print('predicted=%f, expected=%f' % (yhat, obs))
-error = mean_squared_error(test, predictions)
-print('Test MSE: %.3f' % error)
+# Need temp file to store the past history of 2 minutes
+print(type(query_result))
+temp = 
 
-pyplot.plot(test)
-pyplot.plot(predictions, color='red')
-pyplot.show()
 
-# Write on the csv file later
-filename = "sensor_speed.csv"
+# Run ARIMA model
 
-with open(filename, 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-
-    csvwriter.writerow(fields)
-
-    csvwriter.writerows(rows)
+# Write on the json file later
 
 # Update temp file here
